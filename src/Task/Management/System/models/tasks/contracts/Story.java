08c4b8f8d81bff0bcfd514f45b1f2907711f7962
TihomirDimitrov95 <tihomir.dimitrov.1995@abv.bg>
2021-12-17 17:34:39 +0200

    Refactoring Tasks

--- src/Task/Management/System/models/tasks/contracts/Story.java
+++ src/Task/Management/System/models/tasks/contracts/Story.java
@@ -2,7 +2,7 @@ package Task.Management.System.models.tasks.contracts;
 
 import Task.Management.System.models.tasks.enums.*;
 
-public interface Story extends BugsAndStoryIntermediate {
+public interface Story extends Assignable {
 
     void setStatus(StoryStatus status);
 
